64e765f213ffbba7663b01b0ae1f83dc99b56495
Mahmoud Al-Qudsi <mqudsi@neosmart.net>
2016-12-17 04:07:59 -0600

    Initial project creation

---
+++ betterpad/Form1.Designer.cs
@@ -0,0 +1,181 @@
+﻿namespace betterpad
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.toolStripStatusLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.toolStripStatusLabel2 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.toolStripStatusLabel3 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.editToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.formatToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.viewToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.helpToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.text = new betterpad.PaddedRichTextBox();
+            this.statusStrip1.SuspendLayout();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.ImageScalingSize = new System.Drawing.Size(28, 28);
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripStatusLabel1,
+            this.toolStripStatusLabel2,
+            this.toolStripStatusLabel3});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 714);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(1006, 35);
+            this.statusStrip1.TabIndex = 0;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // toolStripStatusLabel1
+            // 
+            this.toolStripStatusLabel1.Name = "toolStripStatusLabel1";
+            this.toolStripStatusLabel1.Size = new System.Drawing.Size(206, 30);
+            this.toolStripStatusLabel1.Text = "toolStripStatusLabel1";
+            // 
+            // toolStripStatusLabel2
+            // 
+            this.toolStripStatusLabel2.Name = "toolStripStatusLabel2";
+            this.toolStripStatusLabel2.Size = new System.Drawing.Size(579, 30);
+            this.toolStripStatusLabel2.Spring = true;
+            this.toolStripStatusLabel2.Text = "toolStripStatusLabel2";
+            // 
+            // toolStripStatusLabel3
+            // 
+            this.toolStripStatusLabel3.Name = "toolStripStatusLabel3";
+            this.toolStripStatusLabel3.Size = new System.Drawing.Size(206, 30);
+            this.toolStripStatusLabel3.Text = "toolStripStatusLabel3";
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(28, 28);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem,
+            this.editToolStripMenuItem,
+            this.formatToolStripMenuItem,
+            this.viewToolStripMenuItem,
+            this.helpToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(1006, 38);
+            this.menuStrip1.TabIndex = 1;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(56, 34);
+            this.fileToolStripMenuItem.Text = "&File";
+            // 
+            // editToolStripMenuItem
+            // 
+            this.editToolStripMenuItem.Name = "editToolStripMenuItem";
+            this.editToolStripMenuItem.Size = new System.Drawing.Size(60, 34);
+            this.editToolStripMenuItem.Text = "&Edit";
+            // 
+            // formatToolStripMenuItem
+            // 
+            this.formatToolStripMenuItem.Name = "formatToolStripMenuItem";
+            this.formatToolStripMenuItem.Size = new System.Drawing.Size(90, 34);
+            this.formatToolStripMenuItem.Text = "F&ormat";
+            // 
+            // viewToolStripMenuItem
+            // 
+            this.viewToolStripMenuItem.Name = "viewToolStripMenuItem";
+            this.viewToolStripMenuItem.Size = new System.Drawing.Size(69, 34);
+            this.viewToolStripMenuItem.Text = "&View";
+            // 
+            // helpToolStripMenuItem
+            // 
+            this.helpToolStripMenuItem.Name = "helpToolStripMenuItem";
+            this.helpToolStripMenuItem.Size = new System.Drawing.Size(68, 34);
+            this.helpToolStripMenuItem.Text = "&Help";
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.ImageScalingSize = new System.Drawing.Size(28, 28);
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(80, 4);
+            // 
+            // text
+            // 
+            this.text.AcceptsTab = true;
+            this.text.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.text.Font = new System.Drawing.Font("Consolas", 11F);
+            this.text.HideSelection = false;
+            this.text.Location = new System.Drawing.Point(0, 38);
+            this.text.Name = "text";
+            this.text.Size = new System.Drawing.Size(1006, 676);
+            this.text.TabIndex = 0;
+            this.text.Text = "Hello, world!";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 24F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1006, 749);
+            this.Controls.Add(this.text);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Betterpad by NeoSmart Technologies";
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel2;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel3;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem editToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem formatToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem viewToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem helpToolStripMenuItem;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private PaddedRichTextBox text;
+    }
+}
+
---
+++ betterpad/Form1.cs
@@ -0,0 +1,174 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.IO.MemoryMappedFiles;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace betterpad
+{
+    public partial class Form1 : Form
+    {
+        private static Dictionary<Keys, Action> _shortcuts;
+        private readonly MemoryMappedFile _mmap = MemoryMappedFile.CreateOrOpen("{6472DD80-A7A5-4F44-BAD4-69BB7F9580DE}", 32);
+        private int _documentNumber;
+        private string _path;
+
+        public Form1()
+        {
+            InitializeComponent();
+            InitializeShortcuts();
+            InitializeLayout();
+            GetDocumentNumber();
+            SetTitle($"Untitled {_documentNumber}");
+        }
+
+        private void GetDocumentNumber()
+        {
+            //purposely not disposing the mmap
+            using (new ScopedMutex(true, "{8BED64DE-A2F9-408F-A223-92EDAD8D90E8}"))
+            using (var view = _mmap.CreateViewAccessor(0, 32))
+            {
+                _documentNumber = view.ReadInt32(0) + 1;
+                view.Write(0, _documentNumber);
+            }
+        }
+
+        private void InitializeShortcuts()
+        {
+            _shortcuts = new Dictionary<Keys, Action>
+            {
+                { Keys.Control | Keys.C, Copy },
+                { Keys.Control | Keys.V, Paste },
+                { Keys.Control | Keys.X, Cut },
+                { Keys.Control | Keys.F, Find },
+                { Keys.Control | Keys.O, Open },
+                { Keys.Control | Keys.S, Save },
+            };
+        }
+
+        private void InitializeLayout()
+        {
+            text.Padding = new Padding(12, 10, 12, 10);
+        }
+
+        protected override bool ProcessCmdKey(ref Message msg, Keys keyData)
+        {
+            if (_shortcuts.TryGetValue(keyData, out var action))
+            {
+                action();
+                return true;
+            }
+
+            // Call the base class
+            return base.ProcessCmdKey(ref msg, keyData);
+        }
+
+        private void Find()
+        {
+            var finder = new FindDialog();
+            finder.ShowDialog(this);
+        }
+
+        private void Paste()
+        {
+            //var data = Clipboard.GetText(TextDataFormat.UnicodeText);
+            text.Paste(DataFormats.GetFormat(DataFormats.UnicodeText));
+        }
+
+        private void Copy()
+        {
+            text.Copy();
+        }
+
+        private void Cut()
+        {
+            text.Cut();
+        }
+
+        private void Open()
+        {
+            var dialog = new OpenFileDialog()
+            {
+                AutoUpgradeEnabled = true,
+                CheckFileExists = true,
+                CheckPathExists = true,
+                DefaultExt = "txt",
+                Filter = "Text Files (*.txt)|*.txt|Log Files (*.log)|*.log",
+                Multiselect = false,
+                RestoreDirectory = true,
+                ShowReadOnly = true,
+                ValidateNames = true,
+                Title = "Open file"
+            };
+
+            if (dialog.ShowDialog(this) == DialogResult.OK)
+            {
+                Open(dialog.FileName);
+            }
+        }
+
+        private void Open(string path)
+        {
+            var dir = Path.GetDirectoryName(path);
+            if (!Directory.Exists(dir))
+            {
+                MessageBox.Show(this, $"Could not find a part of the path {dir}!", "Directory not found!",
+                    MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            _path = path;
+            SetTitle(Path.GetFileName(path));
+
+            if (!File.Exists(path))
+            {
+                //Don't load it, but we will save this reference because we'll write to it
+                return;
+            }
+
+            var data = File.ReadAllText(path);
+            text.Text = data;
+        }
+
+        private void Save()
+        {
+            if (string.IsNullOrEmpty(_path))
+            {
+                var dialog = new SaveFileDialog()
+                {
+                    AddExtension = true,
+                    AutoUpgradeEnabled = true,
+                    CheckFileExists = false,
+                    CheckPathExists = true,
+                    DefaultExt = "txt",
+                    RestoreDirectory = true,
+                    Title = "Save file",
+                    Filter = "Text Files (*.txt)|*.txt|Log Files (*.log)|*.log"
+                };
+
+                if (dialog.ShowDialog(this) == DialogResult.OK)
+                {
+                    _path = dialog.FileName;
+                    Save(_path);
+                }
+            }
+        }
+
+        private void Save(string path)
+        {
+            File.WriteAllText(path, text.Text, Encoding.UTF8);
+        }
+
+        private void SetTitle(string document)
+        {
+            Text = $"{document} - Betterpad by NeoSmart Technologies";
+        }
+    }
+}
